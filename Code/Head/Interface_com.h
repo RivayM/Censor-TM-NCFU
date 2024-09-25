fa5315f60da4b9c9595447dbf067fbd81c4330c7
Tim Romantsov <rivaymark@gmail.com>
2024-09-25 13:52:43 +0300

    commit 9
    Рефакторинг комментариев и кода, приводил в порядок interface_com

--- Code/Head/Interface_com.h
+++ Code/Head/Interface_com.h
@@ -1,6 +1,5 @@
 //********************************************************
-//  Interface common
-//  пародирует I2c или sck + MOSI , настраивается таймером
+//  Interface common (For prog SPI and I2c)
 //********************************************************
 #ifndef Interface_common
 #define Interface_common
@@ -12,23 +11,29 @@
 #define FREQ_480KHZ_L 0xFD
 //********************************************************
 /*USE FREQ IN void init_CLK*/
-#define FREQ_H FREQ_120KHZ_H         // настройка частоты
+#define FREQ_H FREQ_120KHZ_H         // setting freq
 #define FREQ_L FREQ_120KHZ_L
 
-/* void ISR_Timer0() interrupt 1 */  // use
 //********************************************************
-//  для NRF24L01
+//  USE:
+/* 
+void ISR_Timer0() interrupt 1 {
+if(FlagInComSPIGlobal){ InCom_SPI_exchange();}
+}
+*/
 //********************************************************
-#define BUFFER_SPI 8   // количество бит в пакете
-#define BUFFER_SPI_MASSIV_SIZE NRF24_BUFFER_MASSIV_SIZE // количество пакетов( в 1 передаче/приеме)
+//  for NRF24L01
+//********************************************************
+#define BUFFER_SPI 8   // amount bit in packet(byte)
+#define BUFFER_SPI_MASSIV_SIZE NRF24_BUFFER_MASSIV_SIZE // amount packet(byte)
 #define SPI_MSB 1      // or use SPI_LSB = 0
-extern unsigned char valueBufferArrayTx[BUFFER_SPI_MASSIV_SIZE];  // прочитать можно только тогда
-extern unsigned char valueBufferArrayRx[BUFFER_SPI_MASSIV_SIZE];  // когда обмен закончен(размер +1)
+extern unsigned char valueBufferArrayTx[BUFFER_SPI_MASSIV_SIZE];  
+extern unsigned char valueBufferArrayRx[BUFFER_SPI_MASSIV_SIZE];  
 
-extern int counterBit;       // количество бит / использовать в interrupt 1
-extern int counterByte; // количетсво пакетов уже отправленных
-extern int amountByteArrayForSend; // количество байт которое необходимо отправить
-extern bit FlagInComSPIGlobal;  	        // Разрешение ( обмена)
+extern int counterBit;  // current bit in packet(byte)
+extern int counterByte; // current packet(byte)
+extern int amountByteArrayForSend; // current packet(byte) for SEND
+extern bit FlagInComSPIGlobal;  	 // Permission (exchange)
 
 /* USE GPIO.h FOR SPI*/
 #define PIN_CLK_SPI    P10
@@ -36,16 +41,12 @@ extern bit FlagInComSPIGlobal;  	        // Разрешение ( обмена)
 #define PIN_MISO_SPI   P01
 #define PIN_CS_SPI     P15
 
-/*Вспомогательное*/
 void InCom_SPI_init_Timer(void);
 bit InCom_SPI_Output_in_buffer(unsigned char *outSideBuffer);
 void InCom_SPI(bit valueMosi, unsigned char *outSideBuffer );
 void InCom_SPI_CLK_init(bit init);
 void InCom_SPI_Input_in_buffer(unsigned char *outSideBuffer);
-
-/*Обмен*/
 void InCom_SPI_exchange(void);    
 //********************************************************
-
 
 #endif
--- Code/Source/Interface_com.c
+++ Code/Source/Interface_com.c
@@ -4,104 +4,95 @@
 #include <Interface_com.H>
 #include <N76E003.H>
 
-unsigned char valueBufferArrayTx[BUFFER_SPI_MASSIV_SIZE];  // прочитать можно только тогда
-unsigned char valueBufferArrayRx[BUFFER_SPI_MASSIV_SIZE];  // когда обмен закончен(размер +1)
+unsigned char valueBufferArrayTx[BUFFER_SPI_MASSIV_SIZE];  
+unsigned char valueBufferArrayRx[BUFFER_SPI_MASSIV_SIZE];  
 
-int amountByteArrayForSend = BUFFER_SPI_MASSIV_SIZE; // количество байт которое надо отправить
-int counterBit             = 0;   // использовать в interrupt 1
+int amountByteArrayForSend = BUFFER_SPI_MASSIV_SIZE; 
+int counterBit             = 0;   
 int counterByte            = 0;
-bit FlagInComSPIGlobal     = 0;   // 
+bit FlagInComSPIGlobal     = 0;   
 
-/* Инициализация таймера(и его запуск) */
+/* Init timer and start */
 void InCom_SPI_init_Timer(){
-	TIMER0_MODE1_ENABLE;			// Режим - 16 битный таймер
-	TH0 = FREQ_H;           	// настройка частоты
+	TIMER0_MODE1_ENABLE;			// Mode timer
+	TH0 = FREQ_H;           	// setting freq:
   TL0 = FREQ_L; 						// 
 	set_ET0;									// enable Timer0 interrupt
 	set_EA;										// enable interrupts
 	set_TR0;                  // Timer0 run
 }
 
-/* запустить spi обмен пакета /пакетов */
+/* main func(for timer) */
 void InCom_SPI_exchange(void){
 	InCom_SPI( 
 		InCom_SPI_Output_in_buffer(
-			&valueBufferArrayTx[0]),   // буффер для передачи
-		&valueBufferArrayRx[0]);	    // буффер для приема;
+			&valueBufferArrayTx[0]),   // buf  for Tx
+		&valueBufferArrayRx[0]);	   // buf  for Rx
 }
 
-/* начать обмен( чтения и передача) */
+/* exchange bitwise operation */
 void InCom_SPI(bit valueMosi, unsigned char *outSideBuffer){ 
-	int read = 2;                 // количество чтений линии MISO
-	/******************************/
-	if(counterBit == 0) {}  //  CPOL - имитация	 (для slk)
-	else {
-		PIN_CLK_SPI =~ PIN_CLK_SPI; // каждый запуск(в таймере)менять состояние
-	}
-	if( PIN_CLK_SPI == 0) {       //  CPHA - имитация
-	/******************************/
-		switch(counterBit){		// Начало обмена		
-			/*Последний бит пакета*/
-			case BUFFER_SPI:          // Конец обмена 
+	int read = 2;													// amount read MISO
+	if(counterBit == 0) {}  							// CPOL 
+	else {PIN_CLK_SPI =~ PIN_CLK_SPI;} 		// clocking
+	if( PIN_CLK_SPI == 0) {       				// CPHA
+		switch(counterBit){		 							// Start
+			/******************************/
+			case BUFFER_SPI:          				// END
 				PIN_CS_SPI   = 1;
 				PIN_MOSI_SPI = 1;
 				PIN_MISO_SPI = 1;
 				PIN_CLK_SPI  = 0;
 				counterBit = 0;
-				FlagInComSPIGlobal = 0; // Завершить все 
+				FlagInComSPIGlobal = 0; 
 				break;
-			/*Все остальные биты пакета*/
-			default:                            // Идет обмен		
+			/******************************/
+			default:                  				// process going
 				PIN_CS_SPI   = 0;
-				PIN_MOSI_SPI = valueMosi; // отправка
-				
-					if ( PIN_MISO_SPI == 1){       
+				PIN_MOSI_SPI = valueMosi; 			// send MoSi
+					if ( PIN_MISO_SPI == 1){      // send MiSo 	
 						InCom_SPI_Input_in_buffer(outSideBuffer);
 					}	
-				
-				counterBit ++; // следующий бит
+				counterBit ++; // next bit
 				break;
 		}
 	}
 }
 
 
-/* складывать в буфер приема прочитанный бит */
+
+/* save reading bit in buf */
 void InCom_SPI_Input_in_buffer(unsigned char *outSideBuffer){
 	unsigned char buf;
 	buf = *outSideBuffer;
-	if(counterBit == 0){                // начальный бит
+	if(counterBit == 0){         // start bit
 		if(SPI_MSB){
-			*outSideBuffer = 0x80;   // младщий бит - установить на старший
+			*outSideBuffer = 0x80;   
 			return;
 		}
 	}
-	if(counterBit < BUFFER_SPI){
-		if(SPI_MSB){
-			buf = buf + (0x01 << counterBit -1);
-		}
-		else {
-			buf = buf + (0x01 << counterBit);
-		}
+	if(counterBit < BUFFER_SPI){ // next bit
+		if(SPI_MSB)buf = buf + (0x01 << counterBit -1);
+		else buf = buf + (0x01 << counterBit);
 		*outSideBuffer = buf;	
 	}
 }
 
 
-/* читать из буфера передачи и отправить значение*/
+/* read buf and send bit*/
 bit InCom_SPI_Output_in_buffer(unsigned char *outSideBuffer){
 	unsigned char buf;
-	buf = *outSideBuffer;  // сохранить текущее значение буфера для передачи
-	if (counterBit == 0){		      // получить 1 бит для отправки
-		if(SPI_MSB){ buf = buf & 0x01; }  // маска для MSB
-		else       { buf = buf & 0x80; }  // маска для LSB
+	buf = *outSideBuffer;  
+	if (counterBit == 0){	 // start bit
+		if(SPI_MSB){ buf = buf & 0x01; }  // mask MSB
+		else       { buf = buf & 0x80; }  // mask LSB
 	}
-	if( counterBit < 8 && counterBit != 0){	// последующие биты для отправки 
+	if( counterBit < 8 && counterBit != 0){	// next bit 
 		buf = buf & (0x01 << counterBit);
 	}
-	if(buf) { return 1; } else { return 0; } // если 0x00 =>0,а если нет =>1
+	if(buf) { return 1; } else { return 0; } 
 }
 
-/* установить значение clk */
+/* init clk */
 void InCom_SPI_CLK_init(bit init){ PIN_CLK_SPI = init;}
 
